a9c731db98580f25dfc3ee7f1d056c421dc63e90
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-03-19 11:38:59 +0300

    assignment complated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      fetch(`http://localhost:3000/movies/${event.target.searchByID.value}`)
+      .then(response => response.json())
+      .then(data => {
+          const title = document.querySelector('#movieDetails h4');
+          const summary = document.querySelector('#movieDetails p');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+          title.innerHTML = data.title;
+          summary.innerHTML = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
